84cae02b9ad41b43b51484ec68aaa8c76f5b10bc
danielpvpao <danielpvpao@gmail.com>
2024-12-31 11:58:59 -0300

    prisma update

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,11 @@
 import { PrismaClient } from '@prisma/client'
 
-export const prisma = new PrismaClient({ log: ['query'] })
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query'], // Opcional, apenas para logar queries no console
+  })
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
